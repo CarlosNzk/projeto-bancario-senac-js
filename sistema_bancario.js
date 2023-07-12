fb426d49fada0e30692218d683e0796dd0fc03f6
CarlosNzk <139284304+CarlosNzk@users.noreply.github.com>
2023-07-11 21:03:29 -0300

    First Project

---
+++ sistema_bancario.js
@@ -0,0 +1,73 @@
+
+
+let deposito = 0;
+let extrato = '';
+let quantidadeSaque = 0;
+let saqueLimite = 3;
+let saldo = 0;
+
+
+
+while (true) {
+
+    let menu = prompt(`
+====================
+O que você deseja fazer?
+[d]Deposito
+[s]Saque
+[e]Extrato
+[q]Sair
+====================`)
+
+
+    if (menu == 'd') {
+        let valor = parseFloat(prompt('Informe o valor do depósito: '));
+        if (valor > 0) {
+            saldo = saldo + valor
+            extrato = extrato + `
+Deposito no valor de R$ ${valor} realizado.\n`
+            alert(`Deposito no valor de R$${valor} realizado.`)
+        }
+        else {alert('Digite um valor válido!')}
+    }
+    else if (menu == 's') {
+        let valor = parseFloat(prompt('Informe o valor do saque: '));
+        if (quantidadeSaque < saqueLimite && valor <= 500 && valor <= saldo) {
+            saldo = saldo - valor
+            extrato = extrato + `
+Saque no valor de R$ ${valor} realizado.\n`
+            quantidadeSaque = quantidadeSaque + 1
+            alert(`Saque no valor de R$${valor} realizado.`)
+        }
+        else if (valor > saldo) {
+            alert('Saldo insuficiente!')
+        }
+        else if (quantidadeSaque >= saqueLimite) {
+            alert('Você excedeu o limite de 3 saques diários!')
+        }
+        else if (valor > 500) {
+            alert('Máximo de R$ 500,00 por transação')
+        }
+    }
+    else if (menu == 'e') {
+        alert(`
+==============================
+${extrato}
+        
+Seu saldo é de: R$ ${saldo}
+==============================`)
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
